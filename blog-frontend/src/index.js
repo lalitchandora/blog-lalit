c8311e70ae257b7fd857dd1a9939e24a35dbbfd5
Lalit chandora <lalit.n.chandora@gmail.com>
2022-05-10 20:59:16 +0530

    changed index.jsx to index.js

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,5 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
